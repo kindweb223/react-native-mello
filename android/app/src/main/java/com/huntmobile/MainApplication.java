a30f94b3ad408e66bf2ed916ac30bfa52362f73d
Andrei Chmialiou <achimialiou@gmail.com>
2018-09-12 23:58:06 +0300

    FEED-325

--- android/app/src/main/java/com/huntmobile/MainApplication.java
+++ android/app/src/main/java/com/huntmobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.huntmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.poppop.RNReactNativeSharedGroupPreferences.RNReactNativeSharedGroupPreferencesPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNReactNativeSharedGroupPreferencesPackage(),
             new RNFetchBlobPackage(),
             new ImageResizerPackage(),
