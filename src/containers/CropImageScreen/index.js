d7a56f1c330cb8eb8ae6b45ea09c915f7c35073f
kindweb223 <kindwebmobile223@gmail.com>
2018-09-24 07:28:47 +0300

    remove image tag

--- src/containers/CropImageScreen/index.js
+++ src/containers/CropImageScreen/index.js
@@ -32,7 +32,6 @@ class CropImageScreen extends React.Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.user.loading === 'GET_USER_IMAGE_URL_PENDING' && this.props.user.loading === 'GET_USER_IMAGE_URL_FULFILLED') {
       const { userImageUrlData } = this.props.user
-      console.log('GET_USER_IMAGE_URL_FULFILLED: ', userImageUrlData)
       this.uploadImage(userImageUrlData)
     }
 
@@ -46,7 +45,7 @@ class CropImageScreen extends React.Component {
 
     if (prevProps.user.loading === 'UPDATE_PROFILE_PENDING' && this.props.user.loading === 'UPDATE_PROFILE_FULFILLED') {
       this.setState({ loading: false }, () => {
-        this.props.onClose()
+        Actions.pop()
       })
     }
 
@@ -70,11 +69,10 @@ class CropImageScreen extends React.Component {
 
   onSave = () => {
     const { userInfo } = this.props.user
-    // this.setState({ loading: true })
+    this.setState({ loading: true })
     this.imageCrop.crop().then((uri) => {
-      console.log('URL: ', uri)
       this.setState({ cropUrl: uri })
-      // this.props.getImageUrl(userInfo.id)
+      this.props.getImageUrl(userInfo.id)
     })
   }
 
@@ -92,7 +90,7 @@ class CropImageScreen extends React.Component {
               />
             </View>
 
-            <Image
+            {/* <Image
               source={{ uri: this.state.cropUrl.uri }}
               style={{
                 width: 100,
@@ -101,7 +99,7 @@ class CropImageScreen extends React.Component {
                 bottom: 100
               }}
               resizeMode="cover"
-            />
+            /> */}
 
             <View style={styles.headerView}>
               <View style={styles.closeButton} />
