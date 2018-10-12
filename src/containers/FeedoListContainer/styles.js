2b44e69a254c746086c6c4e583752a925acb4391
kindweb223 <kindwebmobile223@gmail.com>
2018-10-12 16:49:10 +0300

    fix: dashboard separator

--- src/containers/FeedoListContainer/styles.js
+++ src/containers/FeedoListContainer/styles.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: COLORS.LIGHT_GREY_LINE,
     marginTop: 16,
-    marginBottom: 10
+    marginBottom: 11
   }
 })
 
