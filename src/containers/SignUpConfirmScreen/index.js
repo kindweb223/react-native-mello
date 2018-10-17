2d83e4d5ab1361f8b263494cb6e4f0245c9f4254
Eamon Doyle <eamon@solvers.io>
2018-10-17 18:45:25 +0100

    Remove test emails Back on SignUp confirm to Actions.LoginStartScreen replace Update password reset text

--- src/containers/SignUpConfirmScreen/index.js
+++ src/containers/SignUpConfirmScreen/index.js
@@ -25,7 +25,7 @@ class SignUpConfirmScreen extends React.Component {
       <TouchableOpacity 
         style={styles.btnBack}
         activeOpacity={0.6}
-        onPress={() => Actions.pop()}
+        onPress={() => Actions.LoginStartScreen({type: 'replace'})}
       >
         <Ionicons name="ios-arrow-back" size={30} color={COLORS.PURPLE} />
       </TouchableOpacity>
@@ -124,7 +124,7 @@ class SignUpConfirmScreen extends React.Component {
 }
 
 SignUpConfirmScreen.defaultProps = {
-  userEmail: 'data-seed@gmail.com'
+  userEmail: ''
 }
 
 SignUpConfirmScreen.propTypes = {
