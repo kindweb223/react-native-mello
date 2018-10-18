dbf9a66f65ab5f5997b6da4ead4d661dc6cc7075
Andrei Chmialiou <achimialiou@gmail.com>
2018-10-18 14:11:01 +0200

    restore the index.js file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import Share from './src/share';
+import {name as appName, nameEx} from './app.json';
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(nameEx, () => Share);
--- index_share.js
+++
@@ -1,4 +0,0 @@
-import {AppRegistry} from 'react-native';
-import Share from './src/share';
-import {nameEx} from './app.json';
-AppRegistry.registerComponent(nameEx, () => Share);
