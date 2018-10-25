262e3b3336163252182e36129e063987b00fc3cd
Andrei Chmialiou <achimialiou@gmail.com>
2018-10-25 10:40:38 +0200

    fixed svg issue

--- android/app/src/main/java/com/feedo/MainApplication.java
+++ android/app/src/main/java/com/feedo/MainApplication.java
@@ -3,7 +3,6 @@ package com.feedo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.apsl.versionnumber.RNVersionNumberPackage;
@@ -36,7 +35,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
             new LottiePackage(),
             new ReactVideoPackage(),
             new RNVersionNumberPackage(),
