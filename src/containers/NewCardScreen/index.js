8d90a94b4151639d140d825983d2d1ae2a312693
Andrei Chmialiou <achimialiou@gmail.com>
2018-10-31 00:04:08 +0100

    Fixed additional issues on FEED-614

--- src/containers/NewCardScreen/index.js
+++ src/containers/NewCardScreen/index.js
@@ -557,6 +557,7 @@ class NewCardScreen extends React.Component {
       }
 
       if (filteredUrls.length > 0) {
+        this.onHideKeyboard();
         // this.isOpenGraphForNewCard = false;
         this.indexForOpenGraph = 0;
         this.openGraphLinksInfo = [];
@@ -569,11 +570,13 @@ class NewCardScreen extends React.Component {
 
   parseErrorUrls(message) {
     const allUrls = message.match(/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi);
+    console.log('allUrls : ', allUrls);
     if (allUrls) {
       let newUrls = [];
       allUrls.forEach(url => {
         const index = _.findIndex(this.parsingErrorLinks, errorLink => errorLink === url);
         if (index === -1) {
+          console.log('Url : ', url);
           url = url.replace('[', '');
           url = url.replace(']', '');
           newUrls.push(url);
@@ -587,12 +590,6 @@ class NewCardScreen extends React.Component {
           //     })
           //   }
           // }
-          if (!this.state.idea.includes(url)) {
-            this.setState({
-              idea: this.state.idea + this.state.idea ? ' ' : '' + url,
-            })
-          }
-          
         }
       });
       if (newUrls.length > 0) {
