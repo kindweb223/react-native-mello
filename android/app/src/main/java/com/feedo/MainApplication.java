30aa4486de142ebd09ae046d00b9af4be7c907e4
kindweb223 <kindwebmobile223@gmail.com>
2018-11-08 00:17:53 +0300

    FEED_633: Added frebase package in xcode as pod

--- android/app/src/main/java/com/feedo/MainApplication.java
+++ android/app/src/main/java/com/feedo/MainApplication.java
@@ -3,6 +3,7 @@ package com.feedo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.bugsnag.BugsnagReactNative;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNDeviceInfo(),
             BugsnagReactNative.getPackage(),
             new LottiePackage(),
