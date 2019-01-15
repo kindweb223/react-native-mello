cf477fef4d4dc39698da40706f5bcc42a2dbadbe
henry191 <henrycalderon191@gmail.com>
2019-01-15 13:52:38 +0300

    FEED_995: update menu style and link share preference

--- src/components/FeedControlMenuComponent/index.js
+++ src/components/FeedControlMenuComponent/index.js
@@ -3,17 +3,29 @@ import {
   View,
   Text,
   TouchableOpacity,
-  FlatList
+  FlatList,
+  Switch
 } from 'react-native'
 import PropTypes from 'prop-types'
 import Octicons from 'react-native-vector-icons/Octicons'
 import Entypo from 'react-native-vector-icons/Entypo'
+import SVGImage from 'react-native-remote-svg'
 import styles from './styles'
 import * as COMMON_FUNC from '../../service/commonFunc'
+import { images } from '../../themes'
+import colors from '../../service/colors'
 
 class FeedControlMenuComponent extends React.Component {
+
+  handleSwitchValue = (value) => {
+    const { handleLinkSharing } = this.props
+    handleLinkSharing(value)
+  }
+
   render() {
     const { feedo, pinText } = this.props
+    let isEnableShare = feedo.sharingPreferences.level === 'INVITEES_ONLY' ? false : true
+    let pinImg = pinText === 'Pin' ? images.pinGrey : images.pinActive
 
     let MENU_ITEMS = []
     if (COMMON_FUNC.isFeedOwner(feedo)) {
@@ -30,37 +42,81 @@ class FeedControlMenuComponent extends React.Component {
 
     return (
       <View style={styles.menuContainer}>
-        <TouchableOpacity onPress={() => this.props.handleSettingItem(pinText)}>
-          <View style={styles.buttonView}>
-            <Octicons name="pin" style={styles.pinIcon} />
-            <Text style={styles.buttonText}>{pinText}</Text>
-          </View>
+        <TouchableOpacity
+          style={styles.settingItem}
+          onPress={() => this.props.handleSettingItem('AddPeople')}
+        >
+          <SVGImage source={images.addProfile} style={styles.menuIcon} />
+          <Text style={styles.settingButtonText}>Add people</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => this.props.handleSettingItem('Share')}>
-          <View style={[styles.buttonView, styles.buttonShareView]}>
-            <Entypo name="share-alternative" style={styles.shareIcon} />
-            <Text style={styles.buttonText}>Share</Text>
-          </View>
+        <TouchableOpacity
+          style={styles.settingItem}
+        >
+          <SVGImage source={images.shareLink} style={styles.menuIcon} />
+          <Text style={styles.settingButtonText}>Link sharing</Text>
+          <Switch
+            style={{ marginLeft: 20 }}
+            trackColor={{true: colors.PURPLE, false: null}}
+            value={isEnableShare}
+            onValueChange={value => this.handleSwitchValue(value)}
+          />
         </TouchableOpacity>
+
+        {
+          isEnableShare &&
+          <TouchableOpacity
+            style={styles.settingItem}
+            onPress={() => this.props.handleSettingItem('ShareLink')}
+          >
+            <SVGImage source={images.shareLink} style={[styles.menuIcon, { opacity: 0 }]} />
+            <Text style={styles.settingButtonText}>Share link</Text>
+          </TouchableOpacity>
+        }
+
+        <View style={styles.separator} />
+
+        <TouchableOpacity
+          style={styles.settingItem}
+          onPress={() => this.props.handleSettingItem(pinText)}
+        >
+          <SVGImage source={pinImg} style={styles.menuIcon} />
+          <Text style={styles.settingButtonText}>{pinText}</Text>
+        </TouchableOpacity>
+
+        <View style={styles.separator} />
 
         <FlatList
           data={MENU_ITEMS}
           keyExtractor={item => item}
           scrollEnabled={false}
           enableEmptySections
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() => this.props.handleSettingItem(item)}
-              activeOpacity={0.5}
-            >
-              <View style={styles.settingItem}>
-                <Text style={(item === 'Delete' || item === 'Leave Flow') ? styles.deleteButtonText : styles.settingButtonText}>
-                  {item}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )}
+          renderItem={({ item }) => {
+            let iconSource
+            if (item === 'Duplicate') {
+              iconSource = images.duplicate
+            } else if (item === 'Edit') {
+              iconSource = images.edit
+            } else if (item === 'Archive') {
+              iconSource = images.archive
+            } else if (item === 'Delete') {
+              iconSource = images.delete
+            }
+
+            return (
+              <TouchableOpacity
+                onPress={() => this.props.handleSettingItem(item)}
+                activeOpacity={0.5}
+              >
+                <View style={styles.settingItem}>
+                  <SVGImage source={iconSource} style={styles.menuIcon} />
+                  <Text style={(item === 'Delete' || item === 'Leave Flow') ? styles.deleteButtonText : styles.settingButtonText}>
+                    {item}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            )
+          }}
         />
       </View>
     )
