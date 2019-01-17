e0a46e05515b13571ad3591138afa8e7c55c1120
henry191 <henrycalderon191@gmail.com>
2019-01-17 21:59:33 +0300

    FEED_997: WIP: tapping member

---
+++ src/components/ContactRemoveComponent/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { View, Modal, TouchableOpacity } from 'react-native'
+import PropTypes from 'prop-types'
+
+import InviteeItemComponent from '../../components/LinkShareModalComponent/InviteeItemComponent'
+import styles from './styles'
+import * as COMMON_FUNC from '../../service/commonFunc'
+import Button from '../Button'
+import COLORS from '../../service/colors'
+
+class ContactRemove extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    const { isRemoveModal, selectedContact, onRemove } = this.props
+    return (
+      <Modal
+        isVisible={isRemoveModal}
+        style={{ margin: 0 }}
+        backdropColor='#e0e0e0'
+        backdropOpacity={0.9}
+        animationIn="slideInUp"
+        animationOut="slideOutDown"
+        animationInTiming={500}
+        onBackdropPress={() => this.props.onRemove()}
+      >
+        <View style={styles.overlay}>
+          {selectedContact &&
+            <InviteeItemComponent
+              invitee={selectedContact}
+              hideLike={false}
+            />
+          }
+          <Button
+            style={styles.button}
+            color='rgba(255, 0, 0, 0.1)'
+            labelColor={COLORS.RED}
+            label="Remove"
+          />
+        </View>
+      </Modal>
+    )
+  }
+}
+
+ContactRemove.propTypes = {
+  isRemoveModal: PropTypes.bool,
+  selectedContact: PropTypes.object,
+  onRemove: PropTypes.func
+}
+
+export default ContactRemove
