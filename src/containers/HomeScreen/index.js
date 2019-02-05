c75e0ccd94e354b8913dc874807f6dfff0a855ad
kindweb223 <kindwebmobile223@gmail.com>
2019-02-05 16:16:34 +0300

    FEED_933: filter flows only for new card

--- src/containers/HomeScreen/index.js
+++ src/containers/HomeScreen/index.js
@@ -293,7 +293,7 @@ class HomeScreen extends React.Component {
           )
         })
 
-        if (feedo.loading !== 'UPDATE_CARD_FULFILLED') {
+        if (feedo.loading !== 'UPDATE_CARD_FULFILLED' || !feedo.isCreateCard) {
           feedoList = orderBy(
             filter(feedoList, item => item.status === 'PUBLISHED'),
             ['publishedDate'],
