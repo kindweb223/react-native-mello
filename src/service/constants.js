5d20650922fa4efb0227d9651e5c375eb91e0f1a
Eamon Doyle <eamon@solvers.io>
2019-02-09 00:15:59 +0000

    Fix merge conflict

--- src/service/constants.js
+++ src/service/constants.js
@@ -83,7 +83,7 @@ export default {
   MODAL_OK: 1,
   MODAL_CLOSE: 2,
 
-  PREMIUM_10MB_ALERT_MESSAGE: 'Oops you need to have a premium account to upload more than 10MB.'
+  PREMIUM_10MB_ALERT_MESSAGE: 'Oops you need to have a premium account to upload more than 10MB.',
 
   // file path in DocumentDirectoryPath
   PRIVACY_POLICE: '/MELLO_PRIVACY_POLICY.txt',
