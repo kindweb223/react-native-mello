5175696b60f1b1082662bf65a770f6998f3439ee
Eamon Doyle <eamon@solvers.io>
2019-02-10 11:09:20 +0000

    Add press in / out effect

--- src/components/FeedCardComponent/index.js
+++ src/components/FeedCardComponent/index.js
@@ -1,19 +1,81 @@
 import React from 'react'
+import { Animated, TouchableHighlight } from 'react-native'
 import FeedCardListComponent from './FeedCardListComponent'
 import FeedCardExtendComponent from './FeedCardExtendComponent'
+import PropTypes from 'prop-types'
 
 class FeedCardComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.animatedPlusButton = new Animated.Value(1);
+  }
+
+  onPressInAddFeed() {
+    const { longHold } = this.props
+
+    if (!longHold) {
+      Animated.timing(this.animatedPlusButton, {
+        toValue: 0.95,
+        duration: 100,
+      }).start();  
+    }
+  }
+
+  onPressOutAddFeed() {
+    Animated.timing(this.animatedPlusButton, {
+      toValue: 1,
+      duration: 100,
+    }).start();
+  }
+
   render() {
     if (this.props.listType === 'LIST') {
       return (
-        <FeedCardListComponent {...this.props} />
+        <TouchableHighlight
+          activeOpacity={1}
+          underlayColor="#fff"
+          onPressIn={this.onPressInAddFeed.bind(this)}
+          onPressOut={this.onPressOutAddFeed.bind(this)}
+          onPress={() => this.props.onPress()}
+          onLongPress={() => this.props.onLongPress()}
+        >
+          <Animated.View
+            style={[{ transform: [{ scale: this.animatedPlusButton }] }]}
+          >
+            <FeedCardListComponent {...this.props} />
+          </Animated.View>
+        </TouchableHighlight>
       )
     } else {
       return (
-        <FeedCardExtendComponent {...this.props} />
+        <TouchableHighlight
+          underlayColor="#fff"
+          onPressIn={this.onPressInAddFeed.bind(this)}
+          onPressOut={this.onPressOutAddFeed.bind(this)}
+          onPress={() => this.props.onPress()}
+          onLongPress={() => this.props.onLongPress()}
+        >
+          <Animated.View
+            style={[{ transform: [{ scale: this.animatedPlusButton }] }]}
+          >
+            <FeedCardExtendComponent {...this.props} />
+          </Animated.View>
+        </TouchableHighlight>
       )
     }
   }
+}
+
+FeedCardComponent.defaultProps = {
+  onLongPress: () => {},
+  onPress: () => {}
+}
+
+FeedCardComponent.propTypes = {
+  longHold: PropTypes.bool,
+  onLongPress: PropTypes.func,
+  onPress: PropTypes.func
 }
 
 export default FeedCardComponent
