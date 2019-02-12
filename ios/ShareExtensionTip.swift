f00354758bf8be57f849f8ca53eda05ddd6c5d0d
Andrei <achimialiou@gmail.com>
2019-02-12 10:50:18 -0600

    FEED_1152: Native Share Extension Tip Styling

--- ios/ShareExtensionTip.swift
+++ ios/ShareExtensionTip.swift
@@ -6,7 +6,11 @@ import UIKit
 
 class ShareExtensionTip: UIView {
   
-  @objc var onUpdate: RCTDirectEventBlock?
+  let numbersize = 18
+  let fontsize = CGFloat(14)
+  let textcolor = UIColor.gray 
+  let backgroundcolor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.00)
+  
   let tipView: UIView = UIView()
   
   override init(frame: CGRect) {
@@ -14,26 +18,26 @@ class ShareExtensionTip: UIView {
 
     let view1: UIView = UIView()
     view1.frame=CGRect(x: 9, y:190, width:357, height:130)
-    view1.backgroundColor = UIColor.white
-    view1.layer.masksToBounds = true
-    view1.layer.cornerRadius = 5
+    view1.backgroundColor = backgroundcolor
+    view1.layer.cornerRadius = 12
     
     let labelNum1: UILabel = UILabel()
     let labelTxt1: UILabel = UILabel()
     let image1 = UIImage(named: "dot3.png")
     let imageView1 = UIImageView(image: image1!)
-    labelNum1.frame = CGRect(x: 10, y:15, width:20, height:20)
+    labelNum1.frame = CGRect(x: 10, y:21, width:numbersize, height:numbersize)
     labelNum1.text = "1"
-    labelNum1.backgroundColor = UIColor.gray
+    labelNum1.font = labelNum1.font.withSize(fontsize)
+    labelNum1.backgroundColor = UIColor.lightGray
     labelNum1.textColor = UIColor.white
     labelNum1.textAlignment = .center
     labelNum1.layer.masksToBounds = true
-    labelNum1.layer.cornerRadius = 10
-    labelTxt1.frame=CGRect(x: 40, y:15, width:260, height:20)
+    labelNum1.layer.cornerRadius = CGFloat(numbersize/2)
+    labelTxt1.frame=CGRect(x: 40, y:19, width:260, height:20)
     labelTxt1.text = "Swipe the top row and tap More"
-    labelTxt1.font = labelTxt1.font.withSize(16)
-    labelTxt1.textColor = UIColor.darkGray
-    imageView1.frame = CGRect(x: 320, y:10, width:30, height:30)
+    labelTxt1.font = labelTxt1.font.withSize(fontsize)
+    labelTxt1.textColor = textcolor
+    imageView1.frame = CGRect(x: 320, y:14, width:30, height:30)
     view1.addSubview(labelNum1)
     view1.addSubview(labelTxt1)
     view1.addSubview(imageView1)
@@ -42,18 +46,19 @@ class ShareExtensionTip: UIView {
     let labelTxt2: UILabel = UILabel()
     let image2 = UIImage(named: "switch.png")
     let imageView2 = UIImageView(image: image2!)
-    labelNum2.frame = CGRect(x: 10, y:50, width:20, height:20)
+    labelNum2.frame = CGRect(x: 10, y:56, width:numbersize, height:numbersize)
     labelNum2.text = "2"
-    labelNum2.backgroundColor = UIColor.gray
+    labelNum2.font = labelNum2.font.withSize(fontsize)
+    labelNum2.backgroundColor = UIColor.lightGray
     labelNum2.textColor = UIColor.white
     labelNum2.textAlignment = .center
     labelNum2.layer.masksToBounds = true
-    labelNum2.layer.cornerRadius = 10
-    labelTxt2.frame=CGRect(x: 40, y:50, width:260, height:20)
+    labelNum2.layer.cornerRadius = CGFloat(numbersize/2)
+    labelTxt2.frame=CGRect(x: 40, y:54, width:260, height:20)
     labelTxt2.text = "Enable Mello and drag it to the top"
-    labelTxt2.font = labelTxt1.font.withSize(16)
-    labelTxt2.textColor = UIColor.darkGray
-    imageView2.frame = CGRect(x: 320, y:45, width:30, height:30)
+    labelTxt2.font = labelTxt1.font.withSize(fontsize)
+    labelTxt2.textColor = textcolor
+    imageView2.frame = CGRect(x: 320, y:52, width:35, height:30)
     view1.addSubview(labelNum2)
     view1.addSubview(labelTxt2)
     view1.addSubview(imageView2)
@@ -61,20 +66,21 @@ class ShareExtensionTip: UIView {
     
     let labelNum3: UILabel = UILabel()
     let labelTxt3: UILabel = UILabel()
-    let image3 = UIImage(named: "AppIcon")
+    let image3 = UIImage(named: "melloicon.png")
     let imageView3 = UIImageView(image: image3!)
-    labelNum3.frame = CGRect(x: 10, y:85, width:20, height:20)
+    labelNum3.frame = CGRect(x: 10, y:91, width:numbersize, height:numbersize)
     labelNum3.text = "3"
-    labelNum3.backgroundColor = UIColor.gray
+    labelNum3.font = labelNum3.font.withSize(fontsize)
+    labelNum3.backgroundColor = UIColor.lightGray
     labelNum3.textColor = UIColor.white
     labelNum3.textAlignment = .center
     labelNum3.layer.masksToBounds = true
-    labelNum3.layer.cornerRadius = 10
-    labelTxt3.frame=CGRect(x: 40, y:85, width:260, height:20)
+    labelNum3.layer.cornerRadius = CGFloat(numbersize/2)
+    labelTxt3.frame=CGRect(x: 40, y:89, width:260, height:20)
     labelTxt3.text = "Tap Mello in the share panel"
-    labelTxt3.font = labelTxt3.font.withSize(16)
-    labelTxt3.textColor = UIColor.darkGray
-    imageView3.frame = CGRect(x: 320, y:80, width:30, height:30)
+    labelTxt3.font = labelTxt3.font.withSize(fontsize)
+    labelTxt3.textColor = textcolor
+    imageView3.frame = CGRect(x: 320, y:84, width:30, height:30)
     view1.addSubview(labelNum3)
     view1.addSubview(labelTxt3)
     view1.addSubview(imageView3)
@@ -98,8 +104,8 @@ class ShareExtensionTip: UIView {
     
     let items = ["Mello"]
     let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
-    ac.view.backgroundColor = UIColor.clear
-    ac.view.tintColor = UIColor.clear
+    ac.view.backgroundColor = backgroundcolor
+    ac.view.tintColor = backgroundcolor
     
     ac.completionWithItemsHandler = {
       (activity, success, items, error) in
