23634593a694dcbda2bdd5b2e179305fb2551978
Andrei <achimialiou@gmail.com>
2019-02-15 15:35:36 -0600

    FEED_1153: Working on Android Share Extenion

--- android/app/src/main/java/io/solvers/feedo/MainApplication.java
+++ android/app/src/main/java/io/solvers/feedo/MainApplication.java
@@ -3,6 +3,7 @@ package io.solvers.feedo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.meedan.ShareMenuPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import com.proyecto26.inappbrowser.RNInAppBrowserPackage;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ShareMenuPackage(),
             new RNGoogleSigninPackage(),
             new RNReactNativeHapticFeedbackPackage(),
             new RNInAppBrowserPackage(),
