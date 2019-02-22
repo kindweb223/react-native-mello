44cb7c1f1d79ddf099e4be473c496ad22f88c427
kindweb223 <kindwebmobile223@gmail.com>
2019-02-22 20:53:48 +0300

    FEED_1198: Added current feed in new card

--- src/containers/CardNewScreen/index.js
+++ src/containers/CardNewScreen/index.js
@@ -731,11 +731,9 @@ class CardNewScreen extends React.Component {
       // If prev page not 'card' or if we don't have a current feed
       // Create feed will be called for a new feed
       // Card will be added on successful response to create feed
-      if (this.props.prevPage !== 'card' || !this.props.feedo.currentFeed.id) {
+      if (this.props.prevPage !== 'card' && (!this.props.feedo.currentFeed.id || this.props.feedo.currentFeed.status === 'TEMP')) {
         this.props.createFeed();
-      }
-      // Otherwise add the card straight to the current feed we have
-      else {
+      } else {  // Otherwise add the card straight to the current feed we have
         this.props.createCard(this.props.feedo.currentFeed.id)
       }
     } else if (viewMode === CONSTANTS.CARD_NEW) {
@@ -1490,6 +1488,7 @@ class CardNewScreen extends React.Component {
     if (cardMode !== CONSTANTS.MAIN_APP_CARD_FROM_DASHBOARD) {
       return;
     }
+
     return (
       <View style={styles.selectFeedoContainer}>
         <Text style={styles.textCreateCardIn}>Create card in:</Text>
