0ed8dd1ed9a02272da62a86252d381b783366878
kindweb223 <kindwebmobile223@gmail.com>
2019-03-05 16:06:37 +0300

    FEED_1295: Fixed disappearing cards from pubnub

--- src/components/FeedCardComponent/FeedCardListComponent/index.js
+++ src/components/FeedCardComponent/FeedCardListComponent/index.js
@@ -61,7 +61,7 @@ class FeedCardListComponent extends React.Component {
               </View>
             )}
 
-            {idea.idea.length > 0 && (
+            {idea.idea && idea.idea.length > 0 && (
               <View style={styles.subView}>
                 <Autolink
                   style={styles.title}
@@ -77,7 +77,7 @@ class FeedCardListComponent extends React.Component {
             )}
           </View>
 
-          {idea && (
+          {idea && idea.metadata && (
             <View style={styles.commentView}>
               <LikeComponent
                 idea={idea}
