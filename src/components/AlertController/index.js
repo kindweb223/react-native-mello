bea00f13697ae5ec9e74303aac0b712ec55bc2dc
Aram Miquel Mateu <arammiquelmateu@Arams-MacBook-Pro.local>
2019-03-19 14:25:24 +0000

    FEED-1286 - Added extra protection

--- src/components/AlertController/index.js
+++ src/components/AlertController/index.js
@@ -3,36 +3,34 @@ import { Alert, AlertButton } from 'react-native'
 
 export default class AlertController {
     static shared = AlertController.shared == null ? new AlertController() : this.shared
-
     isShown = false
-
-    showAlert(title, text) {
-        if (this.isShown) { return }
-        this.isShown = true
-        Alert.alert(title, text, [{ text: 'OK', onPress: () => this.didDismiss() }])
-    }
 
     showAlert(title, text, actions) {
         if (this.isShown) { return }
-        
+
         this.isShown = true
-        newActions = []
 
-        actions.forEach(element => {
-            newActions.push({ 
-                text: element.text, 
-                onPress: () => { 
-                    if (element.onPress != null) { 
-                        element.onPress()
-                    }
+        if (actions != null) {
+            newActions = []    
 
-                    this.didDismiss()
-                },
-                style: element.style 
-            })
-        });
-
-        Alert.alert(title, text, newActions)
+            actions.forEach(element => {
+                newActions.push({ 
+                    text: element.text, 
+                    onPress: () => { 
+                        if (element.onPress != null) { 
+                            element.onPress()
+                        }
+    
+                        this.didDismiss()
+                    },
+                    style: element.style 
+                })
+            });
+    
+            Alert.alert(title, text, newActions)
+        } else {
+            Alert.alert(title, text, [{ text: 'OK', onPress: () => this.didDismiss() }])
+        }
     }
 
     // Call this when using custom actions, to reset state
