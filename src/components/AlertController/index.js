666b2696bacce41eee553f1c39589933df1978ae
Eamon Doyle <eamon@solvers.io>
2019-03-24 15:41:16 +0000

    Wrap in a short timeout so differnet alerts can be shown one after the other

--- src/components/AlertController/index.js
+++ src/components/AlertController/index.js
@@ -6,31 +6,34 @@ export default class AlertController {
     isShown = false
 
     showAlert(title, text, actions) {
-        if (this.isShown) { return }
+        // Wrap in a short timeout so differnet alerts can be shown one after the other
+        setTimeout(() => {
+            if (this.isShown) { return }
 
-        this.isShown = true
-
-        if (actions != null) {
-            newActions = []    
-
-            actions.forEach(element => {
-                newActions.push({ 
-                    text: element.text, 
-                    onPress: () => { 
-                        if (element.onPress != null) { 
-                            element.onPress()
-                        }
+            this.isShown = true
     
-                        this.didDismiss()
-                    },
-                    style: element.style 
-                })
-            });
+            if (actions != null) {
+                newActions = []    
     
-            Alert.alert(title, text, newActions)
-        } else {
-            Alert.alert(title, text, [{ text: 'OK', onPress: () => this.didDismiss() }])
-        }
+                actions.forEach(element => {
+                    newActions.push({ 
+                        text: element.text, 
+                        onPress: () => { 
+                            if (element.onPress != null) { 
+                                element.onPress()
+                            }
+        
+                            this.didDismiss()
+                        },
+                        style: element.style 
+                    })
+                });
+        
+                Alert.alert(title, text, newActions)
+            } else {
+                Alert.alert(title, text, [{ text: 'OK', onPress: () => this.didDismiss() }])
+            }
+          }, 50);
     }
 
     // Call this when using custom actions, to reset state
